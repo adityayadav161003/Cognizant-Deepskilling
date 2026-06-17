7efad583da6997385f2b947b79fb835d9d65838b
adityayadav161003 <142830675+adityayadav161003@users.noreply.github.com>
2026-06-17 11:20:00 +0530

    Implement Engineering concepts - DesignPatterns - Singleton Pattern exercise solution and documentation

---
+++ deepskilling/Engineering concepts/DesignPatterns/Singleton_Pattern/Logger.java
@@ -0,0 +1,9 @@
+public class Logger {
+    private static Logger instance;
+    private Logger() {}
+    public static synchronized Logger getInstance() {
+        if (instance == null) instance = new Logger();
+        return instance;
+    }
+    public void log(String msg) { System.out.println("[LOG]: " + msg); }
+}

abdc1a3912943272db8738d6f1172ac9bbfa7086
adityayadav161003 <142830675+adityayadav161003@users.noreply.github.com>
2026-06-24 11:40:00 +0530

    Implement Module 1-JavaScript - DOM Manipulation exercise solution and documentation

---
+++ upskilling/Module 1-JavaScript/DOM_Manipulation/main.js
@@ -0,0 +1,4 @@
+let card = document.createElement("div");
+card.className = "eventCard";
+card.innerHTML = "<h3>Live Javascript Event</h3><p>Created dynamically</p>";
+document.body.appendChild(card);

8eb4c6d3e0eac134d35aa91dd4ecdd53af21e8bc
adityayadav161003 <142830675+adityayadav161003@users.noreply.github.com>
2026-06-29 10:00:00 +0530

    Implement Module 3-Core Java - DataTypeDemonstration exercise solution and documentation

---
+++ upskilling/Module 3-Core Java/DataTypeDemonstration/Q06_DataTypeDemonstration.java
@@ -0,0 +1,15 @@
+public class Q06_DataTypeDemonstration {
+    public static void main(String[] args) {
+        int intVar = 42;
+        float floatVar = 3.14f;
+        double doubleVar = 2.7182818284;
+        char charVar = 'J';
+        boolean booleanVar = true;
+        
+        System.out.println("Integer value: " + intVar);
+        System.out.println("Float value: " + floatVar);
+        System.out.println("Double value: " + doubleVar);
+        System.out.println("Character value: " + charVar);
+        System.out.println("Boolean value: " + booleanVar);
+    }
+}
